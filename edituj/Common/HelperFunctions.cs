5ea5b39558da0fedee244c38180b144d6c1c6d4b
djape007 <bpredrag96@gmail.com>
2018-11-09 19:56:39 +0100

    mini refaktorisanje

--- edituj/Common/HelperFunctions.cs
+++ edituj/Common/HelperFunctions.cs
@@ -77,10 +77,150 @@ namespace Common
                 Console.WriteLine("8 MaxIncomeByCountry");
                 Console.WriteLine("0 Izidji");
                 Console.Write(">");
-                option = int.Parse(Console.ReadLine());
+
+                string txtInput = Console.ReadLine().Trim();
+
+                if (txtInput.Length == 0) {
+                    Console.WriteLine("Unesi komandu...");
+                    continue;
+                }
+
+                try {
+                    option = int.Parse(txtInput);
+                } catch {
+                    option = -1;
+                }
             }
 
             return option;
+        }
+
+        public static string ReadDatabaseName() {
+            Console.WriteLine("Unesi naziv baze:");
+            string name = Console.ReadLine().Trim();
+
+            while (name.Length == 0) {
+                Console.WriteLine("Unesi naziv baze opet:");
+                name = Console.ReadLine().Trim();
+            }
+
+            return name;
+        }
+
+        public static string ReadCountry() {
+            Console.WriteLine("Unesi naziv drzave:");
+            string name = Console.ReadLine().Trim();
+
+            while (name.Length == 0) {
+                Console.WriteLine("Unesi naziv drzave opet:");
+                name = Console.ReadLine().Trim();
+            }
+
+            if (name.ToLower() == "kosovo") {
+                Console.WriteLine("drzava ne postoji");
+            }
+
+            return name;
+        }
+
+        public static string ReadCity() {
+            Console.WriteLine("Unesi naziv grada:");
+            string name = Console.ReadLine().Trim();
+
+            while (name.Length == 0) {
+                Console.WriteLine("Unesi naziv grada opet:");
+                name = Console.ReadLine().Trim();
+            }
+
+            return name;
+        }
+
+        public static void ExecuteCommand(IMainService proxy, int op) {
+            string name = "";
+            if (op != 0) {
+                name = HelperFunctions.ReadDatabaseName();
+            }
+            switch (op) {
+                case 1:
+                    CheckIfExecuted(proxy.CreateDB(name));
+                    break;
+                case 2:
+                    CheckIfExecuted(proxy.DeleteDB(name));
+                    break;
+                case 3:
+                    Element tmpElem = Element.LoadFromConsole();
+                    CheckIfExecuted(proxy.WriteDB(name, tmpElem));
+                    break;
+                case 4:
+                    List<Element> elems = proxy.ReadDB(name);
+                    Console.WriteLine("Ids of all elements:");
+                    DisplayAllElements(elems, true);
+
+                    Element toEdit = GetElementToEdit(elems);
+
+                    Element newElem = Element.LoadFromConsole();
+                    newElem.Id = toEdit.Id;
+                    CheckIfExecuted(proxy.EditDB(name, newElem));
+                    break;
+                case 5:
+                    DisplayAllElements(proxy.ReadDB(name));
+                    break;
+                case 6:
+                    string city = HelperFunctions.ReadCity();
+                    Console.Write("Prosecna plata za grad " + city + ": ");
+                    Console.WriteLine(proxy.MedianMonthlyIncomeByCity(name, city));
+                    break;
+                case 7:
+                    string country = HelperFunctions.ReadCountry();
+                    Console.WriteLine("Unesi godinu:");
+                    int year = int.Parse(Console.ReadLine());
+                    float medianMonthlyIncome = proxy.MedianMonthlyIncome(name, country, year);
+                    Console.WriteLine("Prosecna plata za " + country + " u " + year + " god.:" + medianMonthlyIncome);
+                    break;
+                case 8:
+                    var tmpDict = proxy.MaxIncomeByCountry(name);
+                    Console.WriteLine("Najveca plata za svaku drzavu:");
+                    foreach (KeyValuePair<string, Element> kvp in tmpDict) {
+                        Console.WriteLine(kvp.Key + " : id:" + kvp.Value.Id + " plata:" + kvp.Value.Income);
+                    }
+                    break;
+                case 0:
+                    Console.WriteLine("Cao poz");
+                    break;
+            }
+        }
+
+        public static void DisplayAllElements(List<Element> elements, bool idOnly = false) {
+            foreach (Element tmpE in elements) {
+                if (idOnly) {
+                    Console.WriteLine(tmpE.Id);
+                } else {
+                    Console.WriteLine(tmpE);
+                }
+            }
+        }
+
+        private static Element GetElementToEdit(List<Element> allElements) {
+            Element toEdit = null;
+            while (toEdit == null) {
+                Console.WriteLine("Unesi id elementa koji menjas:");
+                int idEditElem = int.Parse(Console.ReadLine());
+                toEdit = allElements.Find(x => x.Id == idEditElem);
+
+                if (toEdit == null) {
+                    Console.WriteLine("Ne postoji element sa tim ID-em");
+                }
+            }
+
+            return toEdit;
+        }
+
+        private static void CheckIfExecuted(bool success) {
+            if (success) {
+                Console.WriteLine("Komanda uspesno izvrsena");
+            } else {
+                Console.WriteLine("Komanda nije izvrsena");
+            }
         }
 
         public static string StringPermissionFromAction(string action)
